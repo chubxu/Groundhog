1c6d4920dc0f3090a046dd23be08b429052ef075
chubxu <chubxu@gmail.com>
2022-08-23 23:42:06 +0800

    feat: 迭代模板列表

--- groundhog-vue-admin/src/views/record-template/template-list/index.vue
+++ groundhog-vue-admin/src/views/record-template/template-list/index.vue
@@ -1,3 +1,264 @@
 <template>
-  aaa
+  <div class="container">
+    <Breadcrumb :items="['record.template', 'record.template.templateLists']" />
+    <a-card class="general-card" :title="$t('record.template.templateLists')">
+      <!-- <a-row>
+        <a-col :flex="1">
+          <a-form
+            :model="formModel"
+            :label-col-props="{ span: 6 }"
+            :wrapper-col-props="{ span: 18 }"
+            label-align="left"
+          >
+            <a-row :gutter="16">
+              <a-col :span="8">
+                <a-form-item
+                  field="number"
+                  :label="$t('searchTable.form.number')"
+                >
+                  <a-input
+                    v-model="formModel.number"
+                    :placeholder="$t('searchTable.form.number.placeholder')"
+                  />
+                </a-form-item>
+              </a-col>
+              <a-col :span="8">
+                <a-form-item field="name" :label="$t('searchTable.form.name')">
+                  <a-input
+                    v-model="formModel.name"
+                    :placeholder="$t('searchTable.form.name.placeholder')"
+                  />
+                </a-form-item>
+              </a-col>
+              <a-col :span="8">
+                <a-form-item
+                  field="contentType"
+                  :label="$t('searchTable.form.contentType')"
+                >
+                  <a-select
+                    v-model="formModel.contentType"
+                    :options="contentTypeOptions"
+                    :placeholder="$t('searchTable.form.selectDefault')"
+                  />
+                </a-form-item>
+              </a-col>
+              <a-col :span="8">
+                <a-form-item
+                  field="filterType"
+                  :label="$t('searchTable.form.filterType')"
+                >
+                  <a-select
+                    v-model="formModel.filterType"
+                    :options="filterTypeOptions"
+                    :placeholder="$t('searchTable.form.selectDefault')"
+                  />
+                </a-form-item>
+              </a-col>
+              <a-col :span="8">
+                <a-form-item
+                  field="createdTime"
+                  :label="$t('searchTable.form.createdTime')"
+                >
+                  <a-range-picker
+                    v-model="formModel.createdTime"
+                    style="width: 100%"
+                  />
+                </a-form-item>
+              </a-col>
+              <a-col :span="8">
+                <a-form-item
+                  field="status"
+                  :label="$t('searchTable.form.status')"
+                >
+                  <a-select
+                    v-model="formModel.status"
+                    :options="statusOptions"
+                    :placeholder="$t('searchTable.form.selectDefault')"
+                  />
+                </a-form-item>
+              </a-col>
+            </a-row>
+          </a-form>
+        </a-col>
+        <a-divider style="height: 84px" direction="vertical" />
+        <a-col :flex="'86px'" style="text-align: right">
+          <a-space direction="vertical" :size="18">
+            <a-button type="primary" @click="search">
+              <template #icon>
+                <icon-search />
+              </template>
+              {{ $t('searchTable.form.search') }}
+            </a-button>
+            <a-button @click="reset">
+              <template #icon>
+                <icon-refresh />
+              </template>
+              {{ $t('searchTable.form.reset') }}
+            </a-button>
+          </a-space>
+        </a-col>
+      </a-row>
+      <a-divider style="margin-top: 0" /> -->
+      <a-row style="margin-bottom: 16px">
+        <a-col :span="16">
+          <a-space>
+            <a-button type="primary">
+              <template #icon>
+                <icon-plus />
+              </template>
+              {{ $t('searchTable.operation.create') }}
+            </a-button>
+            <!-- <a-upload action="/">
+              <template #upload-button>
+                <a-button>
+                  {{ $t('searchTable.operation.import') }}
+                </a-button>
+              </template>
+            </a-upload> -->
+          </a-space>
+        </a-col>
+        <!-- <a-col :span="8" style="text-align: right">
+          <a-button>
+            <template #icon>
+              <icon-download />
+            </template>
+            {{ $t('searchTable.operation.download') }}
+          </a-button>
+        </a-col> -->
+      </a-row>
+      <a-table row-key="id" :loading="loading" :pagination="pagination" :data="renderData" :bordered="false" @page-change="onPageChange">
+        <template #columns>
+          <a-table-column :title="$t('templateLists.column.templateId')" data-index="number"/>
+          <a-table-column :title="$t('templateLists.column.templateName')" data-index="name"/>
+          <a-table-column :title="$t('templateLists.column.templateDescription')" data-index="name"/>
+          <a-table-column :title="$t('templateLists.column.templateType')" data-index="name"/>
+          <a-table-column :title="$t('templateLists.column.createdTime')" data-index="createdTime"/>
+          <a-table-column :title="$t('templateLists.column.updateTime')" data-index="updateTime"/>
+          <a-table-column :title="$t('templateLists.column.createdUser')" data-index="createdUser"/>
+          <a-table-column :title="$t('templateLists.column.updateUser')" data-index="updateUser"/>
+          <a-table-column :title="$t('templateLists.columns.operations')" data-index="operations">
+            <template #cell>
+              <a-button v-permission="['admin']" type="text" size="small">
+                {{ $t('templateLists.columns.operations.edit') }}
+              </a-button>
+            </template>
+          </a-table-column>
+        </template>
+      </a-table>
+    </a-card>
+  </div>
 </template>
+
+<script lang="ts" setup>
+  import { computed, ref, reactive } from 'vue';
+  import { useI18n } from 'vue-i18n';
+  import useLoading from '@/hooks/loading';
+  import { queryPolicyList, PolicyRecord, PolicyParams } from '@/api/list';
+  import { Pagination } from '@/types/global';
+  import type { SelectOptionData } from '@arco-design/web-vue/es/select/interface';
+
+  const generateFormModel = () => {
+    return {
+      number: '',
+      name: '',
+      contentType: '',
+      filterType: '',
+      createdTime: [],
+      status: '',
+    };
+  };
+  const { loading, setLoading } = useLoading(true);
+  const { t } = useI18n();
+  const renderData = ref<PolicyRecord[]>([]);
+  const formModel = ref(generateFormModel());
+  const basePagination: Pagination = {
+    current: 1,
+    pageSize: 20,
+  };
+  const pagination = reactive({
+    ...basePagination,
+  });
+  const contentTypeOptions = computed<SelectOptionData[]>(() => [
+    {
+      label: t('searchTable.form.contentType.img'),
+      value: 'img',
+    },
+    {
+      label: t('searchTable.form.contentType.horizontalVideo'),
+      value: 'horizontalVideo',
+    },
+    {
+      label: t('searchTable.form.contentType.verticalVideo'),
+      value: 'verticalVideo',
+    },
+  ]);
+  const filterTypeOptions = computed<SelectOptionData[]>(() => [
+    {
+      label: t('searchTable.form.filterType.artificial'),
+      value: 'artificial',
+    },
+    {
+      label: t('searchTable.form.filterType.rules'),
+      value: 'rules',
+    },
+  ]);
+  const statusOptions = computed<SelectOptionData[]>(() => [
+    {
+      label: t('searchTable.form.status.online'),
+      value: 'online',
+    },
+    {
+      label: t('searchTable.form.status.offline'),
+      value: 'offline',
+    },
+  ]);
+  const fetchData = async (
+    params: PolicyParams = { current: 1, pageSize: 20 }
+  ) => {
+    setLoading(true);
+    try {
+      const { data } = await queryPolicyList(params);
+      renderData.value = data.list;
+      pagination.current = params.current;
+      pagination.total = data.total;
+    } catch (err) {
+      // you can report use errorHandler or other
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const search = () => {
+    fetchData({
+      ...basePagination,
+      ...formModel.value,
+    } as unknown as PolicyParams);
+  };
+  const onPageChange = (current: number) => {
+    fetchData({ ...basePagination, current });
+  };
+
+  fetchData();
+  const reset = () => {
+    formModel.value = generateFormModel();
+  };
+</script>
+
+<script lang="ts">
+  export default {
+    name: 'SearchTable',
+  };
+</script>
+
+<style scoped lang="less">
+  .container {
+    padding: 0 20px 20px 20px;
+  }
+  :deep(.arco-table-th) {
+    &:last-child {
+      .arco-table-th-item-title {
+        margin-left: 16px;
+      }
+    }
+  }
+</style>
--- groundhog-vue-admin/src/views/record-template/template-list/locale/zh-CN.ts
+++ groundhog-vue-admin/src/views/record-template/template-list/locale/zh-CN.ts
@@ -1,3 +1,13 @@
 export default {
   'record.template.templateLists': '模板列表',
+  'templateLists.column.templateId': '模板编号',
+  'templateLists.column.templateName': '模板名称',
+  'templateLists.column.templateDescription': '模板描述',
+  'templateLists.column.templateType': '模板类型',
+  'templateLists.column.createdTime': '创建时间',
+  'templateLists.column.updateTime': '更新时间',
+  'templateLists.column.createdUser': '创建人',
+  'templateLists.column.updateUser': '更新人',
+  'templateLists.columns.operations': '操作',
+  'templateLists.columns.operations.edit': '编辑'
 };
