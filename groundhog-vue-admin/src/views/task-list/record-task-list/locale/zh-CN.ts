e35b678e13ebd17aee8e23e885ea48945814bb41
chubxu <chubxu@gmail.com>
2022-08-31 22:46:18 +0800

    feat: 绘制录制任务列表页面

--- groundhog-vue-admin/src/views/task-list/record-task-list/locale/zh-CN.ts
+++ groundhog-vue-admin/src/views/task-list/record-task-list/locale/zh-CN.ts
@@ -1,3 +1,15 @@
 export default {
   'record.task.list': '录制任务列表',
+
+  'recordTaskList.column.taskId': '任务ID',
+  'recordTaskList.column.taskName': '任务名称',
+  'recordTaskList.column.taskDescription': '任务描述',
+  'recordTaskList.column.taskType': '任务类型',
+  'recordTaskList.column.createdTime': '创建时间',
+  'recordTaskList.column.updateTime': '更新时间',
+  'recordTaskList.column.createdUser': '创建人',
+  'recordTaskList.column.updateUser': '更新人',
+  'recordTaskList.columns.operations': '操作',
+  'recordTaskList.columns.operations.edit': '编辑',
+  'recordTaskList.columns.operations.reRecord': '重新录制',
 };
